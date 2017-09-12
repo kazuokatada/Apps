87499e9c10d9c5a35af52991af6aea3c2030528f
堅田　一夫 <kazuokatada@gmail.com>
2017-09-12 10:38:38 +0900

    getAccoutntsアップロード

---
+++ test1/getAccounts.php
@@ -0,0 +1,36 @@
+<?php
+// This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
+require_once 'HTTP/Request2.php';
+
+$request = new Http_Request2('https://videobreakdown.azure-api.net/Breakdowns/Api/Partner/Accounts');
+$url = $request->getUrl();
+
+$headers = array(
+    // Request headers
+    'Ocp-Apim-Subscription-Key' => 'fd93f1039fc744edbc56411f896e8461',
+);
+
+$request->setHeader($headers);
+
+$parameters = array(
+    // Request parameters
+);
+
+$url->setQueryVariables($parameters);
+
+$request->setMethod(HTTP_Request2::METHOD_GET);
+
+// Request body
+$request->setBody("{body}");
+
+try
+{
+    $response = $request->send();
+    echo $response->getBody();
+}
+catch (HttpException $ex)
+{
+    echo $ex;
+}
+
+?>
